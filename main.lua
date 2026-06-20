b974908acde74b5e0c9513e16aecc6555cff9162
BK UNK <diogogg609@gmail.com>
2026-06-20 13:42:27 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diogogg609-wq/BK-SCRIPTS-/main/sounds.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diogogg609-wq/BK-SCRIPTS-/main/functions.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diogogg609-wq/BK-SCRIPTS-/main/ui_main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/diogogg609-wq/BK-SCRIPTS-/main/key_system.lua"))()
